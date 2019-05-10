e4ef61e7d86bed569859e57ae97eb390c634171d
James Brotsos <james.brotsos@checkmarx.com>
2019-05-10 02:47:50 +0000

    Add new file

---
+++ scripts/checkmarx/create_osa_artifact.py
@@ -0,0 +1,40 @@
+import sys
+import json
+
+def create_json_file(output):
+
+    file = open("gl-dependency-scanning-report.json","w+")
+
+    file.write(output)
+
+    file.close()
+#-----------------------------------------------------------------------------------------------------
+def convert_json(output):
+    dependReport = {}
+    vulnList = []
+
+
+    for result in output:
+        vulnElement = {}
+
+        vulnElement['tool'] = "CxOSA"
+        vulnElement['message'] = result['description']
+        vulnElement['url'] = result['url']
+        vulnElement['cve'] = result['cveName']
+        vulnElement['file'] = result['sourceFileName']
+        vulnElement['priority'] = result['severity']['name']
+                    
+        vulnList.append(vulnElement)
+
+    return (json.dumps(vulnList))
+
+#-----------------------------------------------------------------------------------------------------
+
+args = sys.argv
+
+osa_report = args[1]
+
+with open(osa_report) as fd:
+	out = convert_json(fd.read())
+
+create_json_file(out)
